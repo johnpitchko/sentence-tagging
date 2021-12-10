46f50a26f9fdfdbd7cf13ce173acd2ae49912fd8
John Pitchko <john.pitchko@icloud.com>
2021-12-10 06:07:50 -0700

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :sentences, only: %i[index create]
+  resources :sentences, only: %i[index create destroy]
   resources :entities, only: %i[create]
 
   root 'sentences#index'
